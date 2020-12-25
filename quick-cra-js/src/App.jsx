b7346fb96a49dd2672d1877a9d6e90c2153a7bdd
wuhao <wuhao.igno@bytedance.com>
2020-12-25 11:26:43 +0800

    [version]: 1.5.2 [fix]: 添加 Antd 的 i18n 配置，默认修改为中文

--- quick-cra-js/src/App.jsx
+++ quick-cra-js/src/App.jsx
@@ -4,15 +4,23 @@ import "./App.css";
 import { StoreProvider } from "@/model";
 import Init from "@/components/Init";
 import Router from "@/routes";
+import { ConfigProvider } from "antd";
+import zhCN from "antd/lib/locale/zh_CN";
+import moment from "moment";
+import "moment/locale/zh-cn";
+
+moment.locale("zh-cn");
 
 function App() {
 
   return (
     <StoreProvider>
       <Init>
-        <Layout>
-          <Router />
-        </Layout>
+        <ConfigProvider locale={zhCN}>
+          <Layout>
+            <Router />
+          </Layout>
+        </ConfigProvider>
       </Init>
     </StoreProvider>
   );
